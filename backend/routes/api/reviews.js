fd3cc82438e0cd8d569255bfb9c6d9be88b4144b
elilionel7 <elilionel7@gmail.com>
2024-02-05 01:58:56 -0600

    Fixed server error

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -15,152 +15,8 @@ const {
 const router = express.Router();
 
 // Get All Reviews of the Current User
-// router.get('/current', requireAuth, async (req, res, next) => {
-//   try {
-//     const reviews = await Review.findAll({
-//       where: { userId: req.user.id },
-//       include: [
-//         {
-//           model: User,
-//           as: 'User',
-//           attributes: ['id', 'firstName', 'lastName'],
-//         },
-//         {
-//           model: Spot,
-//           as: 'Spot',
-//           attributes: [
-//             'id',
-//             'ownerId',
-//             'address',
-//             'city',
-//             'state',
-//             'country',
-//             'lat',
-//             'lng',
-//             'name',
-//             'price',
-//             [
-//               literal(`(
-//                 SELECT url FROM "SpotImages" WHERE
-//                 "SpotImages"."spotId" = "Spot"."id" AND
-//                 "SpotImages"."preview" = true
-//                 LIMIT 1
-//             )`),
-//               'previewImage',
-//             ],
-//           ],
-//         },
-//         {
-//           model: ReviewImage,
-//           attributes: ['id', 'url'],
-//           as: 'ReviewImages',
-//         },
-//       ],
-//     });
-
-//     const formattedReviews = reviews.map((review) => ({
-//       ...review.get({ plain: true }),
-//       createdAt: review.createdAt,
-//       updatedAt: review.updatedAt,
-//     }));
-
-//     res.json({ Reviews: formattedReviews });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
+//
 router.get('/current', requireAuth, async (req, res, next) => {
-  //   try {
-  //     const reviews = await Review.findAll({
-  //       where: { userId: req.user.id },
-  //       include: [
-  //         {
-  //           model: User,
-  //           as: 'User',
-  //           attributes: ['id', 'firstName', 'lastName'],
-  //         },
-  //         {
-  //           model: Spot,
-  //           as: 'Spot',
-  //           attributes: {
-  //             include: [
-  //               'id',
-  //               'ownerId',
-  //               'address',
-  //               'city',
-  //               'state',
-  //               'country',
-  //               'lat',
-  //               'lng',
-  //               'name',
-  //               'price',
-  //               [
-  //                 // Ensure your literal query syntax matches PostgreSQL's expectations
-  //                 literal(`(
-  //                   SELECT "url" FROM "SpotImages" AS "SpotImage" WHERE
-  //                   "SpotImages"."spotId" = "Spot"."id" AND
-  //                   "SpotImages"."preview" IS TRUE
-  //                   LIMIT 1
-  //                 )`),
-  //                 'previewImage',
-  //               ],
-  //             ],
-  //           },
-  //         },
-  //         {
-  //           model: ReviewImage,
-  //           attributes: ['id', 'url'],
-  //           as: 'ReviewImages',
-  //         },
-  //       ],
-  //       raw: true,
-  //       nest: true,
-  //     });
-
-  //     const formattedReviews = reviews.map((review) => ({
-  //       ...review,
-  //       // Assuming createdAt and updatedAt are direct properties of the review
-  //       createdAt: review.createdAt,
-  //       updatedAt: review.updatedAt,
-  //     }));
-
-  //     res.json({ Reviews: formattedReviews });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // });
-
-  // //Add an Image to a Review
-  // router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
-  //   try {
-  //     const { reviewId } = req.params;
-  //     const { url } = req.body;
-
-  //     const review = await Review.findOne({
-  //       where: { id: reviewId, userId: req.user.id },
-  //     });
-  //     if (!review) {
-  //       return res.status(404).json({
-  //         message: "Review couldn't be found",
-  //       });
-  //     }
-
-  //     const maxNumImage = await ReviewImage.count({ where: { reviewId } });
-  //     if (maxNumImage >= 10) {
-  //       return res.status(403).json({
-  //         message: 'Maximum number of images for this resource was reached',
-  //       });
-  //     }
-
-  //     const reviewImage = await ReviewImage.create({ reviewId, url });
-  //     const resData = {
-  //       id: reviewImage.id,
-  //       url: reviewImage.url,
-  //     };
-
-  //     res.status(200).json(resData);
-  //   } catch (error) {
   try {
     const userId = req.user.id; // The authenticated user's ID should be set on the request object
 
@@ -198,6 +54,40 @@ router.get('/current', requireAuth, async (req, res, next) => {
     });
 
     res.status(200).json({ Reviews: reviews });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Add an Image to a Review
+router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
+  try {
+    const { reviewId } = req.params;
+    const { url } = req.body;
+
+    const review = await Review.findOne({
+      where: { id: reviewId, userId: req.user.id },
+    });
+    if (!review) {
+      return res.status(404).json({
+        message: "Review couldn't be found",
+      });
+    }
+
+    const maxNumImage = await ReviewImage.count({ where: { reviewId } });
+    if (maxNumImage >= 10) {
+      return res.status(403).json({
+        message: 'Maximum number of images for this resource was reached',
+      });
+    }
+
+    const reviewImage = await ReviewImage.create({ reviewId, url });
+    const resData = {
+      id: reviewImage.id,
+      url: reviewImage.url,
+    };
+
+    res.status(200).json(resData);
   } catch (error) {
     next(error);
   }
