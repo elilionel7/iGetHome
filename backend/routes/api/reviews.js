6983aad28127fab580d770bb1011e89b38d6035e
elilionel7 <elilionel7@gmail.com>
2024-02-05 00:35:22 -0600

    Made Correction

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -98,9 +98,9 @@ router.get('/current', requireAuth, async (req, res, next) => {
               [
                 // Ensure your literal query syntax matches PostgreSQL's expectations
                 literal(`(
-                  SELECT "url" FROM "SpotImages" WHERE
-                  "SpotImages"."spotId" = "Spot"."id" AND
-                  "SpotImages"."preview" IS TRUE
+                  SELECT "url" FROM "SpotImage" WHERE
+                  "SpotImage"."spotId" = "Spot"."id" AND
+                  "SpotImage"."preview" IS TRUE
                   LIMIT 1
                 )`),
                 'previewImage',
